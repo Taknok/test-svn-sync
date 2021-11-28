1b3e35d8ebeb015666c50dd4548ab41f8c006012
techwinder <techwinder@71f4f73e-3029-49f6-977e-f6bbc94eda0c>
2021-11-28 16:23:42 +0000

    tagging version 6.53

---
+++ tags/v6.53/xflr5/xflr5v6/xflcore/gui_params.h
@@ -0,0 +1,14 @@
+
+
+#pragma once
+
+//#define VERSIONNAME     "xflr5 v6.53"
+
+#define MAJOR_VERSION    6
+#define MINOR_VERSION    53
+
+
+//General
+#define MAXRECENTFILES         8  /**< Defines the maximum number of file names in the recent file list */
+#define SETTINGSFORMAT     37351  /**< A random number which defines the format of the settings file */
+
